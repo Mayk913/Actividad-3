d85805db6ac8315335dba7501a12e79cb5512c7c
Gelangela09 <gpintor@uniguajira.edu.co>
2022-08-25 22:54:05 -0500

    inicio

---
+++ Ejercicio.py
@@ -0,0 +1,10 @@
+# Generar un menu, con N opciones, cominando diccionarios con tuplas, que permita colocar el valor de la opción, 
+# la descripcion de opción y el nombre de al función que ejecutaria dicha opción.
+lista=[]
+##
+noptions=int(input("Cuantas opcines desea ingresar:"))
+###
+
+def add():
+    descripcion=input("Redacte la descripcion de la funcion:")
+    nombre=input("Digite el nombre de la funcion: ")
